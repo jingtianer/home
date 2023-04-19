21ab2046b3aeda84561153ce9e4d63f532a11df8
Jingtianer <1043672911@qq.com>
2023-04-19 21:30:45 +0800

    Wed Apr 19 21:30:45 CST 2023

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,11 @@
 hexo clean
 
 # cannot commit tokens to github
-cp _config.yml _config.yml.template
-sed -i "s/<my token>/`cat token.txt`/g" _config.yml
 git add .
 git commit -m "`date`"
+sed -i "s/<my token>/`cat token.txt`/g" _config.yml
 git push master
+sed -i "s/`cat token.txt`/<my token>/g" _config.yml
 hexo g
 # sed -i 's/github.com\/jingtianer/jingtianer.github.io/g' public/atom.xml
 hexo d
-sed -i "s/`cat token.txt`/<my token>/g" _config.yml
