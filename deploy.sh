b43ba029071b0b0ddee4ec80864957a643f8ae09
Jingtianer <1043672911@qq.com>
2023-04-19 20:49:01 +0800

    Wed Apr 19 20:49:01 CST 2023

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
-cp -r ./source /mnt/d/jingtian/blog_back
-cp *.yml /mnt/d/jingtian/blog_back/
+# cp -r ./source /mnt/d/jingtian/blog_back
+# cp *.yml /mnt/d/jingtian/blog_back/
 hexo clean
+git add .
+git commit -m `date`
+git push
 hexo g
 # sed -i 's/github.com\/jingtianer/jingtianer.github.io/g' public/atom.xml
 hexo d
